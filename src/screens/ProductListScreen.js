f2c4044480c74179146bf0114fb6d209113c180f
Daniel Nicácio <70770484+daniel-nicacio@users.noreply.github.com>
2022-09-04 11:17:03 -0300

    fix: fixes in product list fields

--- src/screens/ProductListScreen.js
+++ src/screens/ProductListScreen.js
@@ -82,9 +82,9 @@ function UserListScreen({ history, match }) {
               >
                 <td>{product._id}</td>
                 <td>{product.name}</td>
-                <td>${product.price}</td>
-                <td>${product.category}</td>
-                <td>${product.brand}</td>
+                <td>{product.price && "$" + product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.brand}</td>
 
                 <td>
                   <LinkContainer to={`/admin/product/${product._id}/edit`}>
